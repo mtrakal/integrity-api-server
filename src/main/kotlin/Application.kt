95497713422a22e6681d67b02b1deb1931482f07
Matej Trakal <mtrakal@gmail.com>
2025-01-20 15:55:17 +0100

    init empty project

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package cz.mtrakal
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureFrameworks()
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/HelloService.kt
@@ -0,0 +1,5 @@
+package cz.mtrakal
+
+fun interface HelloService {
+    fun sayHello()
+}
